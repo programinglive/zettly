cd9225d37e55b646095406db34ecf9a2351978c0
programinglive <mahatmamahardhika200588@gmail.com>
2025-10-21 00:00:48 +0700

    chore: enable lazy-loaded pages to reduce chunk size

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,10 +7,19 @@ import 'zettly-editor/styles';
 
 import { ThemeProvider } from './Components/theme-provider';
 
+const pages = import.meta.glob('./Pages/**/*.jsx');
+
 createInertiaApp({
-    resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
-        return pages[`./Pages/${name}.jsx`];
+    resolve: async (name) => {
+        const importer = pages[`./Pages/${name}.jsx`];
+
+        if (!importer) {
+            throw new Error(`Unknown Inertia page: ${name}`);
+        }
+
+        const module = await importer();
+
+        return module.default;
     },
     setup({ el, App, props }) {
         createRoot(el).render(
